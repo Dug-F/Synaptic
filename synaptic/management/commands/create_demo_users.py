5c3063c8b64e3a52a9a2da8622fbec41d5e55125
DougKF <douglaskforbes@gmail.com>
2024-04-30 14:48:17 +0100

    Corrected create_demo_users to use correct password

--- synaptic/management/commands/create_demo_users.py
+++ synaptic/management/commands/create_demo_users.py
@@ -2,17 +2,13 @@ from django.core.management.base import BaseCommand
 from synaptic.models import User
 from .demo_users import DEMO_USERS
 
-
-user_objects = []
-
 class Command(BaseCommand):
     help = 'Creates generic demo users in the database.'
 
     def handle(self, *args, **options):
-        for user in DEMO_USERS:
-            user_objects.append(User(**user))
         try:
-            User.objects.bulk_create(user_objects)
+            for user in DEMO_USERS:
+                User.objects.create_user(**user)
             self.stdout.write(self.style.SUCCESS('Successfully created demo users in the database.'))
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'An error occurred creating the users: {str(e)}'))
